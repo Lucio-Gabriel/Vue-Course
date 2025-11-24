4e09c9706adb71764b33cd3f8421a7452412c074
Lucio-Gabriel <luciogabrieldeazevedoo@gmail.com>
2025-11-24 17:18:34 -0300

    create data in component

--- src/components/FirstComponent.vue
+++ src/components/FirstComponent.vue
@@ -1,9 +1,21 @@
 <template>
-    <h1>Olá VUE</h1>
+    <div>
+        <h1>Olá VUE</h1>
+        <p>Meu nome é: {{ name }}</p>
+        <p>Profissão: {{ profession }}</p>
+        <p>Idade: {{ age }}</p>
+    </div>
 </template>
 
 <script>
     export default {
-        name: 'FirstComponent'
+        name: 'FirstComponent',
+        data() {
+            return { 
+                name: "Lucio Azevedo",
+                profession: "Desenvolvedor de Software",
+                age: 22
+            }
+        }
     }
 </script>
