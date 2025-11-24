2a94281775cf555061c2547add608f01a4fd8a68
Lucio-Gabriel <luciogabrieldeazevedoo@gmail.com>
2025-11-24 17:38:38 -0300

    create life cycle hooks

---
+++ src/components/LifeCycle.vue
@@ -0,0 +1,24 @@
+<template>
+    <h1>Meu nome é: {{ name }}</h1>
+</template>
+
+<script>
+    export default {
+        name: "LifeCycle",
+        data() {
+            return {
+                name: "Ainda não sei"
+            }
+        },
+        created() {
+            setTimeout(() => {
+                this.name = "Bruce Wayne"
+            }, 1000);
+        },
+        mounted() {
+            setTimeout(() => {
+                this.name = "Jonh Doe"
+            }, 2000);
+        }
+    }
+</script>
