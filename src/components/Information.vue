13eddbd0cbee16b996c726421441d168d974d04e
Lucio-Gabriel <luciogabrieldeazevedoo@gmail.com>
2025-11-24 18:01:03 -0300

    Components

---
+++ src/components/Information.vue
@@ -0,0 +1,17 @@
+<template>
+    <div>
+        <p>Estou trabalhando no momento.</p>
+        <p>Utilizo as seguintes tecnlogias:</p>
+        <ul>
+            <li>PHP</li>
+            <li>Laravel</li>
+            <li>Livewire</li>
+        </ul>
+    </div>
+</template>
+
+<script>
+    export default {
+        name: "Information"
+    }
+</script>
---
+++ src/components/Person.vue
@@ -0,0 +1,25 @@
+<template>
+    <div>
+        <h2>Esta é a descrição da pessoa: {{ name }}</h2>
+        <information />
+        <Form /> 
+    </div>
+</template>
+
+<script>
+    import Information from './Information.vue';
+    import Form from './Form.vue';
+
+    export default {
+        name: "Pessoa",
+        components: {
+            Information,
+            Form
+        },
+        data() {
+            return {
+                name: "Matheus"
+            }
+        }
+    }
+</script>
